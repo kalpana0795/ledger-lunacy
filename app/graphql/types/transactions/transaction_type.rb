e33ff56f50309918b6a791688e52e31f6e1453d7
Kalpana Sharma <kalpana.sharma0795@gmail.com>
2023-07-14 11:56:26 +0530

    Improve the transaction description

--- app/graphql/types/transactions/transaction_type.rb
+++ app/graphql/types/transactions/transaction_type.rb
@@ -11,8 +11,40 @@ module Types
       field :description, String, null: false
 
       def description
-        # TODO: Return a logical description
-        [object['type'], object['source']['description']].join(' from ').humanize
+        case object['type']
+        when 'DEPOSIT'
+          "Deposit from #{ source } #{ requester_str } via #{ object['method'] }"
+        when 'INVESTMENT'
+          "Investment in #{ destination }"
+        when 'REFUND'
+          "Refund from #{ source }"
+        when 'WITHDRAWAL'
+          "Withdrawal by you for #{ destination } via #{ object['method'] }"
+        when 'TRANSFER'
+          description_for_transfer
+        end
+      end
+
+      private
+
+      def destination
+        object['destination']['description']
+      end
+
+      def source
+        (object['source']['description'] || "#{ object['source']['type'] } account").humanize
+      end
+
+      def requester_str
+        "for your #{ object['requester']['type'] }".humanize if object['requester']
+      end
+
+      def description_for_transfer
+        if object['amount'] < 0
+          "Transfer for #{ destination }"
+        else
+          "Transfer from #{ source }"
+        end
       end
     end
   end
--- app/javascript/components/Transaction.jsx
+++ app/javascript/components/Transaction.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { formattedDate, formattedCurrency } from './Formatter' 
 
 export default ({transaction}) => {
+  const negativeAmountModifier = transaction.amount < 0 ? 'transaction_data--negative-amount' : ''
+  const zeroBalanceModifier = transaction.balance == 0 ? 'transaction_data--zero-balance' : ''
   return (
     <tr className='transaction_details'>
       <td className='transaction_data'>{ formattedDate(transaction.date) }</td>
       <td className='transaction_data transaction_data--type'>{ transaction.type }</td>
       <td className='transaction_data'>{ transaction.description }</td>
-      <td className='transaction_data'>{ formattedCurrency(transaction.amount) }</td>
-      <td className='transaction_data'>{ formattedCurrency(transaction.balance) }</td>
+      <td className={ `transaction_data ${negativeAmountModifier}` }>{ formattedCurrency(transaction.amount) }</td>
+      <td className={ `transaction_data ${zeroBalanceModifier}` }>{ formattedCurrency(transaction.balance) }</td>
     </tr>
   )
 };
