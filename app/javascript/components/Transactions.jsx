b8b74902437add4331fd6cd8011b9f7be17360d8
Kalpana Sharma <kalpana.sharma0795@gmail.com>
2023-07-14 11:36:02 +0530

    Integrate Apollo Client

--- app/javascript/components/Transactions.jsx
+++ app/javascript/components/Transactions.jsx
@@ -1,54 +1,27 @@
 import React from 'react';
 import { formattedCurrency } from './Formatter'
 import Transaction from './Transaction'
+import { useQuery, gql } from '@apollo/client'
 
-const data = {
-  transactions: [
-    {
-        "activity_id": "1",
-        "date": "2014-10-01T01:00:29+00:00",
-        "type": "DEPOSIT",
-        "method": "ACH",
-        "amount": 1003.75,
-        "balance": 1003.75,
-        "requester": {
-            "type": "INVESTMENT"
-        },
-        "source": {
-            "type": "EXTERNAL",
-            "id": 18238147,
-            "description": "Chase ** 9867"
-        },
-        "destination": {
-            "type": "INVESTOR",
-            "id": 76510190788,
-            "description": "Michael Daugherty"
-        }
-    },
-    {
-        "activity_id": "2",
-        "date": "2014-10-01T01:00:30+00:00",
-        "type": "INVESTMENT",
-        "amount": -999.50,
-        "balance": 4.25,
-        "requester": {
-            "type": "INVESTMENT"
-        },
-        "source": {
-            "type": "INVESTOR",
-            "id": 76510190788,
-            "description": "Investor description"
-        },
-        "destination": {
-            "type": "CAMPAIGN_FUND",
-            "id": "899b188040fd01315c6206cbae434dcb",
-            "description": "Uber Technologies"
-        }
+const GET_TRANSACTIONS = gql`
+  query transactions {
+    transactions{
+      activityId
+      date
+      amount
+      balance
+      type
+      description
     }
-  ],
-}
+  }
+`
 
-export default () => {
+const Transactions = () => {
+  const {data, loading, error} = useQuery(GET_TRANSACTIONS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error : {error.message}</p>
+
   return (
     <>
       <div className="heading">
@@ -58,7 +31,7 @@ export default () => {
       <section className="balance_info_container">
         <div className="balance_info">
           <h1 className="balance_info_amount">
-            { formattedCurrency(data.transactions.slice(-1)[0].balance) }
+            { formattedCurrency(data.transactions[0].balance) }
           </h1>
           <p className="balance_info_label">balance</p>
         </div>
@@ -80,7 +53,7 @@ export default () => {
               </thead>
               <tbody>
                 { data.transactions.map((transaction) => (
-                  <Transaction transaction={transaction} key={transaction.activity_id} />
+                  <Transaction transaction={transaction} key={transaction.activityId} />
                 ))}
               </tbody>
             </table>
@@ -90,3 +63,5 @@ export default () => {
     </>
   )
 };
+
+export default Transactions
--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
 import Transactions from "./Transactions";
+
+const csrfToken = document
+  .querySelector('meta[name=csrf-token]')
+  .getAttribute('content')
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    credentials: 'same-origin',
+    headers: {
+      'X-CSRF-Token': csrfToken,
+    },
+  }),
+  cache: new InMemoryCache(),
+})
 
 document.addEventListener("turbo:load", () => {
   const root = createRoot(
     document.body.appendChild(document.createElement("div"))
-  );
-  root.render(<Transactions />);
+  )
+  root.render(
+    <ApolloProvider client={client}>
+      <Transactions />
+    </ApolloProvider>,
+  )
 });
